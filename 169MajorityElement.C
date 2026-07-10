7246fb57415449b82308cf82a6f87f4ed7c28f02
Tigas <tiagodesouzagibbon@gmail.com>
2026-07-09 22:42:18 -0300

    169  Boyer Moore

--- 169MajorityElement.C
+++ 169MajorityElement.C
@@ -2,22 +2,19 @@
 #include <stdlib.h>
 
 int majorityElement(int* nums, int numsSize) {
+    int resultado;
+    int count = 0;
 
-    int i;
-    int max = nums[0];
-    int min = nums[0];
-    for ( i = 1; i < numsSize; i++ ) {
-        if ( max < nums[i] ) max = nums[i];
-        if ( min > nums[i] ) min = nums[i];
+    for ( int i = 0; i < numsSize; i++ ) {
+        if ( count == 0 ) {
+            resultado = nums[i];
+            count = 1;
+        } else if ( resultado == nums[i] ) {
+            count++;
+        } else count--;
     }
 
-    int *count = (int*)calloc( max - min + 1, sizeof(int) );
-    for ( i = 0; i < numsSize; i++ ) {
-        count[nums[i] - min]++;
-        if ( count[nums[i] - min] > numsSize/2 ) break;
-    }
-    free ( count );
-    return nums[i];
+    return resultado;
 }
 
 int main() {
@@ -25,14 +22,16 @@ int main() {
     int c1[3] = {3,2,3};
     int c2[7] = {2,2,1,1,1,2,2};
     int c3[6] = {-1,1,1,1,2,1};
+    int c4[3] = {3,3,4};
 
     int correto = 0;
 
     if ( majorityElement ( c1, 3 ) == 3 ) correto++;
     if ( majorityElement ( c2, 7 ) == 2 ) correto++;
     if ( majorityElement ( c3, 6 ) == 1 ) correto++;
+    if ( majorityElement ( c4, 3 ) == 3 ) correto++;
     
-    printf("%d/3 testes corretos.", correto);
+    printf("%d/4 testes corretos.", correto);
 
     return 0;
 }
